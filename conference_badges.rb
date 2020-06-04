37d1fbf2a9e16985957cf62bb42edeaf401326a6
Flora Petersen <floramarina@comcast.net>
2020-06-04 03:19:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def batch_badge_creator(attendees)
+  attendees.collect do |name|
+    badge_maker(name)
+  end
+end
+
+def assign_rooms(attendees)
+  each_with_index.collect do |name, index|
+    return "Hello ${name}! You'll be assigned to room #{index+1}!"
+  end
+end
