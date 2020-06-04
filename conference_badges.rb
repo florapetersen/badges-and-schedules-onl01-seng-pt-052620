6ccab5b7bcdc80111b1953a934f69f022a442047
Flora Petersen <floramarina@comcast.net>
2020-06-04 03:52:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,6 +12,6 @@ end
 
 def assign_rooms(attendees)
   attendees.each_with_index.collect do |name, index|
-    "Hello #{name}! You'll be assigned to room #{index+1}!"
+    "Hello, #{name}! You'll be assigned to room #{index+1}!"
   end
 end
